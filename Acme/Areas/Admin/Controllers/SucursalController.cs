69032ee7824e4bd633990032c4ea4568c893b2f9
joOrch lugardo <jorch_lug@outlook.com>
2023-03-08 10:34:55 -0600

    agregar vista Index Sucursal

---
+++ Acme/Areas/Admin/Controllers/SucursalController.cs
@@ -0,0 +1,35 @@
+﻿using Acme.AccesoDatos.Repositorio.IRepositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Acme.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SucursalController : Controller
+    {
+
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public SucursalController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public IActionResult ObtenerTodos()
+        {
+            var todos = _unidadTrabajo.Sucursal.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+        #endregion
+    }
+}
